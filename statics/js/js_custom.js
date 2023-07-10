1ce502c5aebdbff6e51a69f91945993c7cfab8ed
tuancuongbota <93958224+tuancuongbota@users.noreply.github.com>
2023-07-10 08:00:52 +0700

    fix js

--- statics/js/js_custom.js
+++ statics/js/js_custom.js
@@ -43,16 +43,25 @@ $(document).ready(function(){
     function deactivateSlide(slideIndex) {
       $('#home-title-slide' + slideIndex).removeClass('active');
     }
+    $(window).on('load resize', function() {
+      var windowWidth = $(window).width();
+      
+      if (windowWidth < 768) { 
+        $('.slick-slideshow').slick('slickSetOption', 'variableWidth', true);
+      } else {
+        $('.slick-slideshow').slick('slickSetOption', 'variableWidth', false);
+      }
+    });
+    
     $('.slick-slideshow').slick({
-        dots: true,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        adaptiveHeight: false,
-        autoplay: true,
-        variableWidth: true,
-        autoplaySpeed: 8000,
-        customPaging: (_slider, i) => '<div id="dot' + (i + 1) + '"><svg viewBox="0 0 40 40" class="spinner" width="20" height="20" style="--duration: 15.046667s;"><circle cx="20" cy="20" r="12" class="outer" opacity="0.5"></circle><circle cx="20" cy="20" r="5.5" class="inner" opacity="0.5"></circle></svg></div>'
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      adaptiveHeight: false,
+      autoplay: true,
+      autoplaySpeed: 8000,
+      customPaging: (_slider, i) => '<div id="dot' + (i + 1) + '"><svg viewBox="0 0 40 40" class="spinner" width="20" height="20" style="--duration: 15.046667s;"><circle cx="20" cy="20" r="12" class="outer" opacity="0.5"></circle><circle cx="20" cy="20" r="5.5" class="inner" opacity="0.5"></circle></svg></div>'
     });
   }
   if($('.header-search-toggle').length) {
