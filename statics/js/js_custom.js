367328bf8d24af97f29998e53ada135465f45714
tuancuongbota <tuancuong.bota@gmail.com>
2023-07-11 22:14:35 +0700

    fix slideshow

--- statics/js/js_custom.js
+++ statics/js/js_custom.js
@@ -60,6 +60,10 @@ $(document).ready(function(){
       slidesToShow: 1,
       adaptiveHeight: false,
       autoplay: true,
+      transformsEnabled: !1,
+      unslicked: !1,
+      pauseOnHover: false,
+      pauseOnFocus: false,
       autoplaySpeed: 8000,
       customPaging: (_slider, i) => '<div id="dot' + (i + 1) + '"><svg viewBox="0 0 40 40" class="spinner" width="20" height="20" style="--duration: 15.046667s;"><circle cx="20" cy="20" r="12" class="outer" opacity="0.5"></circle><circle cx="20" cy="20" r="5.5" class="inner" opacity="0.5"></circle></svg></div>'
     });
